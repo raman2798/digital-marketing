d46140cf26a4c10f621bae08d529f758d2d00637
raman2798 <ramanagarwal158@gmail.com>
2024-11-03 23:58:22 +0530

    Prepare for deployment

---
+++ README.md
@@ -0,0 +1,31 @@
+# Digital Marketing
+
+This is the repository for the Digital Marketing.
+
+## Getting Started
+
+### Installation Instructions
+
+1. Clone the repository:
+
+```bash
+git clone https://github.com/raman2798/digital-marketing.git
+
+cd digital-marketing
+```
+
+2. Install node and npm
+
+3. Install dependencies:
+
+```
+npm install
+```
+
+### Starting the Server
+
+To start the server on localhost, run:
+
+```bash
+npm run dev
+```
---
+++ vite.config.ts
@@ -0,0 +1,38 @@
+import { defineConfig, loadEnv } from 'vite';
+import _ from 'lodash';
+import react from '@vitejs/plugin-react';
+import { fileURLToPath } from 'url';
+import { dirname, resolve } from 'path';
+
+// Get the directory name of the current module
+const __filename = fileURLToPath(import.meta.url);
+
+const __dirname = dirname(__filename);
+
+// https://vite.dev/config/
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  const isCustomDomain = _.isEqual(_.get(env, 'VITE_CUSTOM_DEPLOYMENT', false), true);
+
+  const isGenerateSourceMap = _.isEqual(_.get(env, 'VITE_GENERATE_SOURCEMAP', false), true);
+
+  return {
+    plugins: [react()],
+    base: isCustomDomain ? '/digital-marketing/' : '/',
+    build: {
+      sourcemap: isGenerateSourceMap,
+    },
+    resolve: {
+      alias: {
+        '#': resolve(__dirname, './src'),
+        assets: resolve(__dirname, './src/assets'),
+        components: resolve(__dirname, './src/components'),
+        pages: resolve(__dirname, './src/pages'),
+        utils: resolve(__dirname, './src/utils'),
+        types: resolve(__dirname, './src/types'),
+        public: resolve(__dirname, './public'),
+      },
+    },
+  };
+});
